d9fb4a0c7113fbc95843b6ea754dc22804f4377e
Luke Konopka <luke@lkonopka.pl>
2017-04-01 21:16:11 +0200

    project cleanup

--- client/store/index.js
+++ client/store/index.js
@@ -4,24 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  count: 0
+
 }
 
 const mutations = {
-  INCREMENT (state) {
-    state.count++
-  },
-  DECREMENT (state) {
-    state.count--
-  }
+
 }
 
 const actions = {
-  incrementAsync ({ commit }) {
-    setTimeout(() => {
-      commit('INCREMENT')
-    }, 200)
-  }
+
 }
 
 const store = new Vuex.Store({
