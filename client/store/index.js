7a6e1aed1c28cc802965086e69ab18c0372d023d
Luke Konopka <luke@lkonopka.pl>
2017-04-01 21:40:38 +0200

    basic store for question list created

--- client/store/index.js
+++ client/store/index.js
@@ -1,18 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import questions from '../mockData/questions'
+
 Vue.use(Vuex)
 
 const state = {
-
+  questions: [],
+  userDetail: {},
+  questionDetail: {}
 }
 
 const mutations = {
+  addQuestions (state, newQuestions) {
+    state.questions = state.questions.push(newQuestions)
+  },
 
+  setUserDetail (state, userDetail) {
+    state.userDetail = userDetail
+  },
+
+  setQuestionDetail (state, questionDetail) {
+    state.questionDetail = questionDetail
+  }
 }
 
 const actions = {
-
+  fetchQuestions ({ commit }, pageNo) {
+    // mocked data
+    commit('addQuestions', questions[pageNo])
+  }
 }
 
 const store = new Vuex.Store({
