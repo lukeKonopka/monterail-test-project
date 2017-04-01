57b6cbea7206a257904d8589f601cef9468c59dc
Luke Konopka <luke@lkonopka.pl>
2017-04-01 21:41:19 +0200

    mock data for questions added

---
+++ client/mockData/questions.js
@@ -0,0 +1,108 @@
+export default [
+  [
+    {
+      user: {
+        id: 1,
+        name: 'Eva',
+        avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+      },
+      content: {
+        title: 'Will insulin make my patient gain weight?',
+        activities: [
+          {
+            user: {
+              id: 2,
+              name: 'Andrew',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'answer'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Patrick',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Jim',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Scott',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Spock',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          }
+        ]
+      }
+    },
+    {
+      user: {
+        id: 1,
+        name: 'Eva',
+        avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+      },
+      content: {
+        title: 'Vegan diet in diabetes treatment?',
+        activities: [
+          {
+            user: {
+              id: 2,
+              name: 'Andrew',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'answer'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Patrick',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Jim',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Scott',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          },
+          {
+            user: {
+              id: 2,
+              name: 'Spock',
+              avatarUrl: 'https://www.practicepte.com/images/clients/avatar_man.jpg'
+            },
+            type: 'comment'
+          }
+        ]
+      }
+    }
+  ]
+]
