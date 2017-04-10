0c72dbd5894d256b6f83010ed12d5e88445e6b0a
Luke Konopka <luke@lkonopka.pl>
2017-04-10 03:58:39 +0200

    added detail question view

---
+++ client/views/QuestionDetailView.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div class="questionDetailView">
+    <Header>
+      <div slot="top" class="discussedLastTime">
+        <span class="desc">
+          Discussed last time
+        </span>
+        <span class="value">
+          1 day ago
+        </span>
+      </div>
+    </Header>
+    <main>
+      {{$route.params.id}}
+    </main>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.questionDetailView {
+  .discussedLastTime {
+    text-align: right;
+  }
+
+  main {
+    margin-top: 132px;
+  }
+}
+</style>
